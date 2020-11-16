67554aabd9aa59eb79651580dfebef738e10f1dd
Tobias Gurdan <tobias@gurdan.de>
2020-11-17 00:12:14 +0100

    Simplify express server

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-
-const __dirname = path.dirname( fileURLToPath( import.meta.url ));
 
 const app = express();
-app.use( express.static( __dirname + '/public' ));
-app.use( express.static(path.join(__dirname, 'node_modules/' )));
+app.use( express.static( 'public' ));
+app.use( express.static( 'node_modules' ));
 
 app.listen( 3000, () =>
     console.log( 'visit http://localhost:3000' )
